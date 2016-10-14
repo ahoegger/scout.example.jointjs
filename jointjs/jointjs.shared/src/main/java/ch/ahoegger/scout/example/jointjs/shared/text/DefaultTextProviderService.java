d89c886e24de8dec9a101ae2f4f2776cb8be38ec
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2016-10-14 19:08:49 +0200

    Initial project, created with 'New Scout Project' wizard.
    Change-Id: Icce536cc8c2edbeb7dcca9f02dd21495599e38eb

---
+++ jointjs/jointjs.shared/src/main/java/ch/ahoegger/scout/example/jointjs/shared/text/DefaultTextProviderService.java
@@ -0,0 +1,17 @@
+package ch.ahoegger.scout.example.jointjs.shared.text;
+
+import org.eclipse.scout.rt.platform.Order;
+import org.eclipse.scout.rt.shared.services.common.text.AbstractDynamicNlsTextProviderService;
+
+/**
+ * <h3>{@link DefaultTextProviderService}</h3>
+ *
+ * @author aho
+ */
+@Order(-2000)
+public class DefaultTextProviderService extends AbstractDynamicNlsTextProviderService {
+	@Override
+	protected String getDynamicNlsBaseName() {
+		return "ch.ahoegger.scout.example.jointjs.shared.texts.Texts";
+	}
+}
